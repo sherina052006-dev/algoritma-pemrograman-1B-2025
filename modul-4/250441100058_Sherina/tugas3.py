e18408b83f3199d1c178a999de2dfd8d1ad2c082
sherina052006-dev <sherina.052006@gmail.com>
2025-11-04 09:25:36 +0700

    250441100058_Sherina_Modul4_KakDhani

---
+++ modul-4/250441100058_Sherina/tugas3.py
@@ -0,0 +1,13 @@
+n = int(input("Masukkan nilai n: "))
+
+for i in range(1, n + 1):
+    
+    for j in range(1, i + 1):
+        print(j, end='')
+
+    for k in range(2 * (n - i)):
+        print(' ', end='')
+
+    for l in range(i, 0, -1):
+        print(l, end='')
+    print()
